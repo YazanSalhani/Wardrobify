e256f8022936109bbd3c724829f4d867404a9b0b
Yazan <yzan.alsalhani@gmail.com>
2024-07-10 21:10:32 +0200

    fixed the poller, added models and views

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,69 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from hats.api.common.json import ModelEncoder
+from hats.api.hats_rest.models import Hat, LocationVO
+
+
+class LocationVOEncoder(ModelEncoder):
+    model = LocationVO
+    properties = ["import_href"]
+
+class HatsEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "fabric",
+        "style",
+        "color",
+        "picture_url",
+        "location",
+    ]
+    encoders = {
+        "location": LocationVOEncoder(),
+    }
 
 # Create your views here.
+@require_http_methods(["GET", "POST"])
+def api_hats(request):
+    if request.method == "GET":
+        hats = Hat.objects.all()
+        return JsonResponse(
+            {"hats": hats},
+            encoder=HatsEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        hat = Hat.objects.create(**content)
+        return JsonResponse(
+            hat,
+            encoder=HatsEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["DELETE", "GET"])
+def api_hat(request, pk):
+    if request.method == "GET":
+        try:
+            hat = Hat.objects.get(id=pk)
+            return JsonResponse(
+                hat,
+                encoder=HatsEncoder,
+                safe=False
+            )
+        except hat.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
+    else:
+        try:
+            hat = Hat.objects.get(id=pk)
+            hat.delete()
+            return JsonResponse(
+                hat,
+                encoder=HatsEncoder,
+                safe=False,
+            )
+        except Hat.DoesNotExist:
+            return JsonResponse({"message": "Does not exist"})
