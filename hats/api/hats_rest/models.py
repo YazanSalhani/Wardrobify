fd08d9fca5ef74b1a3262b1243bcddd197a2bcfb
Yazan <yzan.alsalhani@gmail.com>
2024-07-11 16:43:14 +0200

    repolished the back end and fixed the POST REQUEST

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -10,7 +10,7 @@ class Hat(models.Model):
     fabric = models.CharField(max_length=200)
     style = models.CharField(max_length=200)
     color = models.CharField(max_length=200)
-    picture_url = models.URLField(null=True)
+    picture_url = models.URLField(null=True, blank=True)
     location = models.ForeignKey(
         LocationVO,
         related_name="hats",
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -24,21 +24,40 @@ class HatsEncoder(ModelEncoder):
 
 # Create your views here.
 @require_http_methods(["GET", "POST"])
-def api_hats(request):
+def api_hats(request, location_vo_id=None):
     if request.method == "GET":
-        hats = Hat.objects.all()
+        if location_vo_id is not None:
+            hats = Hat.objects.filter(location=location_vo_id)
+        else:
+            hats = Hat.objects.all()
         return JsonResponse(
             {"hats": hats},
             encoder=HatsEncoder,
         )
     else:
         content = json.loads(request.body)
+
+        # Get the Conference object and put it in the content dict
+        try:
+            #conference_href = f"/api/conferences/{conference_vo_id}/"
+            location_href = content["location"]
+            location = LocationVO.objects.get(import_href=location_href)
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
+
         hat = Hat.objects.create(**content)
         return JsonResponse(
             hat,
             encoder=HatsEncoder,
             safe=False,
         )
+    ##user input id or href
+    ##find locationvo object to match
+    ##in json reassign the location key to be a locationvo object
 
 
 @require_http_methods(["DELETE", "GET"])
