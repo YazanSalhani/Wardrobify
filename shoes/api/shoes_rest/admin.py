41d1ce68e1925a1d3c4f49b8d6eab9372d5926c7
unknown <Mira>
2024-07-11 12:03:39 -0400

    Functional Shoe Api

--- shoes/api/shoes_rest/admin.py
+++ shoes/api/shoes_rest/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Shoes
 
 # Register your models here.
+@admin.register(Shoes)
+class ShoesAdmin(admin.ModelAdmin):
+    list_display = [
+        "manufacturer",
+        "model_name",
+        "color",
+        "picture_url",
+        "bin",
+    ]
--- wardrobe/api/wardrobe_api/admin.py
+++ wardrobe/api/wardrobe_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Bin
 
 # Register your models here.
+@admin.register(Bin)
+class BinAdmin(admin.ModelAdmin):
+    list_display = [
+        "closet_name",
+        "bin_number",
+        "bin_size",
+    ]
